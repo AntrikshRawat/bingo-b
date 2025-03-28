16cfb5cece963f90f43e1576c63ef764270e574c
Antriksh rawat <antrikshrawat2@gmail.com>
2025-03-28 13:59:15 +0530

    ain

--- src/config/server.js
+++ src/config/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const http = require('http');
 const { Server } = require('socket.io');
 
-const ALLOWED_ORIGINS = ['https://bingo-f.vercel.app', 'http://localhost:3000'];
+const ALLOWED_ORIGINS = ['https://bingo-f.vercel.app', 'http://localhost:5173'];
 const PORT = process.env.PORT || 5000;
 
 const createServer = () => {
