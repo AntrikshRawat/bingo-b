90d11ef9997aa28cd84c0c3d7ca8789f241fd4bf
Antriksh rawat <antrikshrawat2@gmail.com>
2025-04-22 22:10:53 +0530

    bug fiex

--- src/config/server.js
+++ src/config/server.js
@@ -5,11 +5,9 @@ const { Server } = require('socket.io');
 
 const ALLOWED_ORIGINS = ['https://bingo-f.vercel.app', 'http://localhost:5173'];
 const PORT = process.env.PORT || 5000;
-
 const createServer = () => {
   const app = express();
   app.use(cors({ origin: ALLOWED_ORIGINS }));
-  
   const server = http.createServer(app);
   const io = new Server(server, {
     cors: { origin: ALLOWED_ORIGINS }
